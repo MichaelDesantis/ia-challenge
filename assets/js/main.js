af0afd63280e31e1e5fe97d46a992ecc9b8fa4a3
Michael Desantis <multishifties@yahoo.com>
2019-06-02 18:00:24 -0500

    reset working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,22 @@
+/**
+ * Manage Dots and targets
+ */
+
 const dropTargets = document.getElementsByClassName("drop-target");
 const dots = document.getElementsByClassName("color-dot-container");
 
 const dropHandler = (e) => {
     e.preventDefault();
-    console.log(e);
-
     let elementID = e.dataTransfer.getData("elementID");
-    console.log(elementID);
-    console.log(typeof elementID);
+    let color = e.target.getAttribute("data-target-color");
+
+    if (e.target.childElementCount > 0) {
+        return;
+    }
+
+    if (!elementID.includes(color)) {
+        return;
+    }
     e.target.appendChild(document.getElementById(elementID));
 }
 
@@ -15,25 +24,39 @@ const allowDrop = (e) => {
     e.preventDefault();
 }
 
-let currentDot;
+// for (let i = 0; i < dropTargets.length; i++) {
+//     dropTargets[i].addEventListener("dragenter", (e) => {
+//         e.target.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
+//     });
 
-for (let i = 0; i < dropTargets.length; i++) {
-    dropTargets[i].addEventListener("dragenter", (e) => {
-        // e.target.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
-
-    });
-
-    dropTargets[i].addEventListener("dragleave", (e) => {
-        // e.target.style.backgroundColor = "rgba(0, 0, 0, 0.0)";
-    });
-}
+//     dropTargets[i].addEventListener("dragleave", (e) => {
+//         e.target.style.backgroundColor = "rgba(0, 0, 0, 0.0)";
+//     });
+// }
 
 for (let i = 0; i < dots.length; i++) {
     dots[i].addEventListener("dragstart", (e) => {
-        // console.log(e);
-        // console.log(e.target.getAttribute("data-color"));
-
         let elementID = e.target.id;
         e.dataTransfer.setData("elementID", elementID);
     });
 }
+
+/**
+ * Reset
+ */
+const reset = () => {
+    for (let i = 0; i < dropTargets.length; i++) {
+        if (dropTargets[i].childElementCount === 0) {
+            continue;
+        } else {
+            let child = dropTargets[i].children[0];
+            document.querySelectorAll(`[data-child="${child.id}"]`)[0].appendChild(child)
+        }
+    }
+};
+
+document.getElementById("reset").addEventListener("click" , reset);
+
+ /**
+  * Modal
+  */
